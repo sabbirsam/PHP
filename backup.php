75808feeacec1fcf7bb0a420f8925326183c9c09
sabbirsam <61382787+sabbirsam@users.noreply.github.com>
2021-05-03 23:52:39 +0600

    ++ and -- operator

--- backup.php
+++ backup.php
@@ -55,3 +55,103 @@ printf("MY %s name is %s %s","Full",$fname,$lname);//output: MY Full name is Sab
 /**
  * Printf use ================================================================Start
  */
+
+
+ /**
+  * addition substraction and all=============================================Start
+  */
+
+$a = 12;
+$a+= 1;
+$b = 6;
+$c=$a+$b;
+printf("Sum of %s is ",$a);
+echo "\n";
+printf("Sum of %s and %s is %s",$a,$b,$a+$b);
+echo "\n";
+printf("Sum of %s and %s is %s",$a,$b,$a-$b);
+echo "\n";
+printf("Sum of %s and %s is %s",$a,$b,$a*$b);
+echo "\n";
+printf("Sum of %s and %s is %s",$a,$b,$a/$b);
+echo "\n";
+printf("Sum of %s and %s is %s",$a,$b,$a%$b);
+echo "\n";
+printf("Sum of %s and %s is %s",$a,$b,$c);
+echo "\n";
+
+
+$a = 12;
+$b=1;
+$b+=$a+2;
+echo ($a);
+echo"\n";
+echo ($b);
+
+
+
+$a = 12;
+$b=1;
+
+$a++;
+$b--;
+echo "\n";
+echo $a;
+echo "\n";
+$a--;
+echo "\n";
+echo $a;
+echo "\n";
+
+echo $b;
+
+/**
+  * addition substraction and all=============================================End
+  */
+
+
+// assignment operator with increment and decrement\
+
+/**
+ * $m = $n then 
+ * $n = $n+1  
+ * so output : m= 1, n=2
+ */
+$n = 1;
+$m= $n++; //first m will be assign on n then  increase then n 
+echo $m, "\n", $n; // meabs $m = $n then $n = $n+1  so output : m= 1, n=2
+
+
+echo "\n";
+
+/**
+ *  
+ * $n = $n+1 
+ * $m = $n then 
+ * so output : m= 1, n=2
+ */
+$n = 1;
+$m= ++$n;  //first n will increase then m = n
+
+$n = 1;
+$m= ++$n;
+
+echo $m, "\n", $n;// $n= $n + 1 $n then $m=$n  //so output 2 2
+
+
+/**
+ * m = n then
+ * n=m - 1
+ */
+$n = 1;
+$m= $n--;
+echo $m, " \n",$n;
+
+
+//n=n-1 then m = n   so 0  0
+$n = 1;
+$m= --$n;
+
+//n=n-1 then m = n   so 0  0
+echo $m ,"\n" , $n;
+
--- demo.php
+++ demo.php
@@ -1,13 +1,19 @@
 <?php
 
-$name = "Sabbir Ahmed";
-$fname= "Sabbir";
-$lname= "Ahmed";
-$uname = strtoupper($name);
-//%s is only work on printf 
-//printf is need to show funtion easily exm: strtoupper function which is not possible in this way on echo
-printf("My name is %s",$uname);
-echo"\n";
-printf("My name is %s", strtoupper($name));
-echo"\n";
-printf("MY %s name is %s %s","Full",$fname,$lname);//output: MY Full name is Sabbir Ahmed
+
+$n = 1;
+$m= --$n;
+
+//n=n-1 then m = n   so 0  0
+echo $m ,"\n" , $n;
+
+
+
+
+
+
+
+
+
+
+
