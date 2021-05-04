3d5aa27b78d8e94b1ccb96fd635e98e34c49f157
sabbirsam <61382787+sabbirsam@users.noreply.github.com>
2021-05-04 23:33:41 +0600

    ternary and other if else if switch new way to use

--- demo.php
+++ demo.php
@@ -9,6 +9,50 @@
  if ($n%2 == 0):
     echo "Even";
     echo PHP_EOL; //new line
- else:
+
+elseif (true): 
+    
+else:
     echo "Odd";
- endif;
+endif;
+
+
+
+/**
+ * If new
+ */
+
+if($n%2==0):
+    ?>
+        even
+    <?php
+else:
+    ?>
+    Odd
+    <?php
+endif;
+      
+  
+
+/**
+ * Switch new
+ */
+
+switch ($n%2 ==0):
+    case 0:
+    echo "Even";
+    echo PHP_EOL; //new line
+    break;
+
+    default:
+    echo "Even";
+endswitch;
+
+
+/**
+ * 
+ * Ternary operator fix
+ */
+$n=10;
+ $result = ($n%2==0) ? "A" : (($n==11) ? "B" : "C");//always use () bracket at the end condition
+ echo $result;
